33f849b2251d7b63446312c481cc1f4491b3e453
Isaac Potoczny-Jones <ijones@tozny.com>
2016-04-26 13:52:17 -0700

    Removed (now unused) runTests script and added banner to @momahajogami script.

--- test.sh
+++ test.sh
@@ -1,3 +1,19 @@
+#!/bin/sh
+
+echo '  ________________________________________
+> / " know that two and two make four - and  \
+> | should be glad to prove it too if I      |
+> | could - though I must say if by any sort |
+> | of process I could convert 2 and 2 into  |
+> | five it would give me much greater       |
+> \ pleasure."  - George Gordon Byron        /
+>   ----------------------------------------
+>          \   ^__^ 
+>           \  (oo)\_______
+>              (__)\       )\/\
+>                  ||----w |
+>                  ||     ||'
+
 python test_poly_divmod.py
 python test_poly_gcd.py
 python test_sieve.py
